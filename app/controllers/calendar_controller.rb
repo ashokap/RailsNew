67838b4b087a214dc05b7240a1c101ed7d09939a
Ashoka Prabhakara <ashoka.p@gmail.com>
2014-11-19 12:58:27 +0530

    Import/Export ics file with validations

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,56 +5,40 @@ class CalendarController < ApplicationController
   end
 
   def import
-    #@calendars = Calendar.all
     @uploaded_io = params[:file]
+    puts("File type:::: #{@uploaded_io.content_type}")
     if params[:file].blank?
-     # render action: 'index', notice: 'Please select a file to import.'
-      flash.now[:notice] = "Saved!"
-    #redirect_to :back
-      #flash[:error] = 'Please select a file to import.'
-    else
-    #puts("File Name: #{@uploaded_io.original_filename}")
+      redirect_to '/calendar/index', :notice => "Please select a file to import."
+    elsif @uploaded_io.content_type == "text/calendar"
+      #puts("File Name: #{@uploaded_io.original_filename}")
       tmpfile=File.open(Rails.root.join('public', @uploaded_io.original_filename), 'wb') do |file|
         file.write(@uploaded_io.read)
       end
-      flash[:notice] = "Import successful"
-    self.parse
+      redirect_to '/calendar/index', :notice => "Import successful."
+      self.parse
+    else
+      redirect_to '/calendar/index', :notice => "Please select .ics file."
     end
-
-    redirect_to '/calendar/index'
   end
 
   def parse
-
-    puts("Uploaded file name: #{@uploaded_io.original_filename}")
-    #@calendars = Calendar.all
     # Open a file or pass a string to the parser
     cal_file = File.open( Rails.root.join('public', @uploaded_io.original_filename), "r")
-
     # Parser returns an array of calendars because a single file
     # can have multiple calendars.
     cals = Icalendar.parse(cal_file)
     #puts("All Calendars: #{cals}")
-
-    i=0
-    j=0
     cals.each do |cal|
     # Now you can access the cal object in just the same way I created it
     # puts("Current Calendar: #{cal}")
       cal.events.each do |event|
-        i+=1
         # puts "Current event : #{event}"
-
         @calendar = Calendar.new(params[:calendar])
         @calendar.assign_attributes(:summary => " #{event.summary}", :starttime => "#{event.dtstart}" , :timezone => "#{event.dtstart.ical_params['tzid']}")
         @calendar.save
       #puts "start date-time timezone: #{event.dtstart.ical_params['tzid']}"
-
       end
-      j+=1
     end
-  #puts "Counters: i #{i}, j #{j}"
-
   end
 
   def export
@@ -69,12 +53,10 @@ class CalendarController < ApplicationController
       calendar.add_event(event)
     #puts("Current Event: #{event}")
     end
-
     calendar.publish
     file = File.new("tmp/sample.ics", "w+")
     file.write(calendar.to_ical)
     file.close
-
     send_file("tmp/sample.ics")
 
   end
