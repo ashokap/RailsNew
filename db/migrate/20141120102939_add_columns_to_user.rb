46b86de1a2d3e260668089be02d3d2457249ee96
Ashoka Prabhakara <ashoka.p@gmail.com>
2014-11-20 16:31:55 +0530

    Google calendar integration-Updated User table

---
+++ db/migrate/20141120102939_add_columns_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :token, :string
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119103554) do
+ActiveRecord::Schema.define(version: 20141120102939) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 20141119103554) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
